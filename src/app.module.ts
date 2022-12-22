1b5caca41606b69286cc218327b6f12bfb473080
Sergei Alimov <dev.sergeialimov@gmail.com>
2022-12-22 10:21:08 +0300

    update imports in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TracksController } from './tracks/tracks.controller';
+import { TracksService } from './tracks/tracks.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TracksController],
+  providers: [AppService, TracksService],
 })
 export class AppModule {}
