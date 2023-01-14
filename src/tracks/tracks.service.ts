109e7188e0b705a9046ff63c80a8004bb1430b67
Sergei Alimov <dev.sergeialimov@gmail.com>
2023-01-14 20:56:08 +0300

    update getAll and getOne

--- src/tracks/tracks.service.ts
+++ src/tracks/tracks.service.ts
@@ -1,17 +1,21 @@
 const tracks = {
   1: {
+    id: 1,
     name: 'Track 1',
     hidden: false,
   },
   2: {
+    id: 2,
     name: 'Track 2',
     hidden: false,
   },
   3: {
+    id: 3,
     name: 'Track 3',
     hidden: false,
   },
   4: {
+    id: 4,
     name: 'Track 4',
     hidden: true,
   },
@@ -19,33 +23,43 @@ const tracks = {
 
 export class TracksService {
   getAll() {
-    return tracks;
+    const tracksArr = Object.values(tracks);
+    return tracksArr.filter((track) => !track.hidden);
   }
 
-  getOne(id: string) {
-    return tracks.find((track) => track.id === +id);
+  getOne(id: number) {
+    if (typeof id !== 'number') {
+      return 'Id should be a number';
+    }
+    if (id <= 0 || id > Object.keys(tracks).length) {
+      return `id ${id} is not correct`;
+    }
+    if (tracks[id].hidden){
+      return `Track ${id} is hidden`;
+    }
+    return tracks[id];
   }
 
   createTrack(name: string) {
-    const id = tracks.length + 1;
-    tracks.push({ id, name });
-    return { id, name };
+    // const id = tracks.length + 1;
+    // tracks.push({ id, name });
+    // return { id, name };
   }
 
   updateTrack(trackId: number, newName: string) {
-    const track = tracks.find(({ id }) => id === +trackId);
-    track.name = newName;
-    return track;
+    // const track = tracks.find(({ id }) => id === +trackId);
+    // track.name = newName;
+    // return track;
   }
 
   deleteTrack(id: number) {
-    console.log(id, typeof id);
-    const index = tracks.findIndex((track) => track.id === id);
-    console.log(index);
-    if (index !== -1 && !tracks[index].hidden) {
-      delete tracks[index];
-      console.log(tracks);
-    }
-    return `Specified song wasn't found`;
+  //   console.log(id, typeof id);
+  //   const index = tracks.findIndex((track) => track.id === id);
+  //   console.log(index);
+  //   if (index !== -1 && !tracks[index].hidden) {
+  //     delete tracks[index];
+  //     console.log(tracks);
+  //   }
+  //   return `Specified song wasn't found`;
   }
 }
